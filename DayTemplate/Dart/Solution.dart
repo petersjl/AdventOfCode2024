61be82f40f2193d2ba67e4dbdc2421df646e7685
Joseph Peters <petersjl@rose-hulman.edu>
2024-12-05 21:40:52 -0500

    Change day template to use typedef for solution input

--- DayTemplate/Dart/Solution.dart
+++ DayTemplate/Dart/Solution.dart
@@ -2,7 +2,7 @@
 
 import '../../Utils/DartUtils.dart';
 
-void main(){
+void main() {
   bool runP1 = true;
   bool runP2 = false;
   String solutionP1 = "", solutionP2 = "";
@@ -11,25 +11,28 @@ void main(){
   var input = parseInput(Utils.readToString("../input.txt"));
   var timeParse = stopwatch.elapsedMilliseconds;
 
-  if(runP1) solutionP1 = solvePart1(input);
+  if (runP1) solutionP1 = solvePart1(input);
   var timeP1 = stopwatch.elapsedMilliseconds;
-  if(runP2) solutionP2 = solvePart2(input);
+  if (runP2) solutionP2 = solvePart2(input);
   var timeP2 = stopwatch.elapsedMilliseconds;
 
-  print('Parse time: ${timeParse * 1/1000}s');
-  if(runP1) print('Part 1 (${(timeP1 - timeParse) * 1/1000}s): ${solutionP1}');
-  if(runP2) print('Part 2 (${(timeP2 - timeP1) * 1/1000}s): ${solutionP2}');
-  print('Ran in ${stopwatch.elapsedMilliseconds * 1/1000} seconds');
+  print('Parse time: ${timeParse * 1 / 1000}s');
+  if (runP1)
+    print('Part 1 (${(timeP1 - timeParse) * 1 / 1000}s): ${solutionP1}');
+  if (runP2) print('Part 2 (${(timeP2 - timeP1) * 1 / 1000}s): ${solutionP2}');
+  print('Ran in ${stopwatch.elapsedMilliseconds * 1 / 1000} seconds');
 }
 
-List<String> parseInput(String input){
+typedef InputType = List<String>;
+
+InputType parseInput(String input) {
   return [];
 }
 
-String solvePart1(List<String> input){
+String solvePart1(InputType input) {
   return input.toString();
 }
 
-String solvePart2(List<String> input) {
+String solvePart2(InputType input) {
   return "";
 }
